37e6fdfa1475d44f64c682524e6461a1f05b2363
Rafael Ribaldo <ribaldorafael@gmail.com>
2017-07-28 17:20:47 -0300

    Adding utils to install_requires list

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url="http://campaignmonitor.github.io/createsend-python/",
     license="MIT",
     keywords="createsend campaign monitor email",
-    packages=['createsend', 'createsend.utils'],
+    packages=['createsend'],
     package_data={'createsend': ['cacert.pem']},
-    install_requires=['six'],
+    install_requires=['six', 'createsend.utils'],
 )
